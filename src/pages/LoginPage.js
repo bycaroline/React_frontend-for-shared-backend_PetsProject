e6892fec8d163a4eab2125d065d30ed5925c9a6e
bycaroline <eklund.caroline@gmail.com>
2024-10-14 13:44:32 +0200

    cleanup

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,6 @@
 import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom'; // Import useNavigate hook from react-router-dom
-// import { login } from '../utilities/auth'; // Import the login function
 
 const LoginPage = () => {
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
-    // const navigate = useNavigate(); // Hook for programmatic navigation
-
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     const success = await login(email, password);
-    //     if (success) {
-    //         navigate('/home'); // Redirect to home page or another route
-    //     } else {
-    //         // Handle login failure
-    //         alert('Login failed. Please check your credentials.');
-    //     }
-    // };
 
     return (
 
@@ -37,23 +21,7 @@ const LoginPage = () => {
                 <button type="button" class="btn btn-info">Submit</button>
             </form>
         </div>
-        // <form onSubmit={handleSubmit}>
-        //     <input
-        //         type="text"
-        //         value={email}
-        //         onChange={(e) => setEmail(e.target.value)}
-        //         placeholder="Email"
-        //         required
-        //     />
-        //     <input
-        //         type="password"
-        //         value={password}
-        //         onChange={(e) => setPassword(e.target.value)}
-        //         placeholder="Password"
-        //         required
-        //     />
-        //     <button type="submit">Login</button>
-        // </form>
+
     );
 };
 
